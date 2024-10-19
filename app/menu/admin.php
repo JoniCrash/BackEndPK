a1775daae75ee4bdbd1ba850917563f770cbe192
JoniCrash <iwan@comet.net.id>
2024-10-19 13:48:06 +0700

    kk

--- app/menu/admin.php
+++ app/menu/admin.php
@@ -5,57 +5,44 @@
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
 
-               <li class="nav-item">
+          <li class="nav-item">
             <a href="index.php?page=dashboard" class="nav-link">
               <i class="nav-icon fas fa-th"></i>
               <p>
                 Dashboard
-                <span class="right badge badge-danger">New</span>
               </p>
             </a>
           </li>
           <li class="nav-item">
-                <a href="index.php?page=user-app" class="nav-link active">
-                  <i class="far fa-circle nav-icon"></i>
+                <a href="index.php?page=user-app" class="nav-link">
+                  <i class="far fa-user nav-icon"></i>
                   <p>Pengguna Aplikasi</p>
                 </a>
               </li>
-          <li class="nav-item menu-open">
-            <!-- <a href="#" class="nav-link active">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Pelanggan
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a> -->
-            <!-- <ul class="nav nav-treeview"> -->
               <li class="nav-item">
                 <a href="index.php?page=data-pelanggan" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
+                  <i class="far fa-user nav-icon"></i>
                   <p>Pelanggan</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="index.php?page=data-paket" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
+                <a href="index.php?page=data-paket" class="nav-link" >
+                  <i class="far fa-folder nav-icon"></i>
                   <p>Paket</p>
                 </a>
               </li>
               <li class="nav-item">
                 <a href="index.php?page=data-pengajuan" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
+                  <i class="far fa-plus nav-icon"></i>
                   <p>Pengajuan</p>
                 </a>
               </li>
               <li class="nav-item">
                 <a href="index.php?page=data-pembayaran" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
+                  <i class="far fa-credit-card nav-icon"></i>
                   <p>Pembayaran</p>
                 </a>
               </li>
-            <!-- </ul> -->
-          </li>
-
           <li class="nav-item">
             <a href="logout.php" class="nav-link text-red">
               <i class="nav-icon fas  fa-power-off"></i>
